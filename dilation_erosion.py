d21f474d95e496c1deae24de20923d109e749960
Forkbabu <sayantandas30011998@gmail.com>
2020-05-06 19:12:13 +0530

    Update dilation_erosion.py

--- dilation_erosion.py
+++ dilation_erosion.py
@@ -79,6 +79,35 @@ class DenMoNet(nn.Module):
                 self.de_layer.number_of_erosions, dset_name)
 
     def forward(self, input: torch.Tensor):
+        print(input.shape)
+        temp = self.de_layer(input)
+        self.temp = temp
+        print(temp.shape)
+        classification = self.linear_combination_layer(temp)
+        print(classification.shape)
+        return classification
+
+    def store(self, dset_name: str, directory: Path):
+        name = self.name(dset_name)
+        fname = "{}.tch".format(name)
+        torch.save(self, str(directory / fname))
+
+class DeroNet(nn.Module):
+    """The dilation-erosion network."""
+
+    def __init__(self, input_space_dim: int, number_dilations: int, number_erosions: int, output_space_dim: int):
+        super().__init__()
+        self.de_layer = DilateErode(input_space_dim, number_dilations, number_erosions)
+        # The linear combination size is the number of erosions plus the number of dilations, plus
+        # one bias node for each (if there's at least one, that is).
+        lc_size = number_erosions + number_dilations
+        self.linear_combination_layer = nn.Linear(lc_size, output_space_dim)
+
+    def name(self, dset_name: str):
+        return "dero_{}x{}_{}".format(self.de_layer.number_of_dilations,
+                self.de_layer.number_of_erosions, dset_name)
+
+    def forward(self, input: torch.Tensor):
         temp = self.de_layer(input)
         self.temp = temp
         classification = self.linear_combination_layer(temp)
@@ -88,3 +117,27 @@ class DenMoNet(nn.Module):
         name = self.name(dset_name)
         fname = "{}.tch".format(name)
         torch.save(self, str(directory / fname))
+        
+        
+        
+class SimpleCNN(nn.Module):
+    def __init__(self, input_channels, input_width, num_classes):
+        super(SimpleCNN, self).__init__()
+        self.dim = input_width
+        self.conv1 = nn.Conv2d(input_channels, 50, kernel_size=3)
+        self.conv2 = nn.Conv2d(50, 100, kernel_size=5)
+        self.conv3 = nn.Conv2d(100, 200, kernel_size=5)
+        self.conv4 = nn.Conv2d(200, 400, kernel_size=2)
+        self.fc1 = nn.Linear(400, 300)
+        self.fc2 = nn.Linear(300, num_classes)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(F.max_pool2d(self.conv2(x), 2))
+        x = F.relu(F.max_pool2d(self.conv3(x), 2))
+        x = F.relu(self.conv4(x), 2)
+        x = x.view(-1, 400)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
